4e379c7ea1811f8cb6b4b06c4da02ffa28cf9bd8
jsg5383 <jsg5383@gmail.com>
2022-01-05 02:59:48 +0900

    no message

--- js/index.js
+++ js/index.js
@@ -7,26 +7,26 @@ $('#loding').fadeOut(2000, function(){
 
 $(document).ready(function() {
     //スクロールイベント
-    $('.page').scroll(function(e){
-        const targetId =  "#" + $(e.target).children().context.id;
-        var scrollTop = $(targetId).scrollTop();
-        var innerHeight = $(targetId).innerHeight();
-        var scrollHeight = $(targetId).prop('scrollHeight');
-        if (scrollTop + innerHeight >= scrollHeight) {
-            if (targetId == '#p1') {
-                location.href='#t2'
-            }
-            if (targetId == '#p2') {
-                location.href='#t3'
-            }
-            if (targetId == '#p3') {
-                location.href='#t4'
-            }
-            return true;
-        } else {
-            return false;
-        }
-    });
+    // $('.page').scroll(function(e){
+    //     const targetId =  "#" + $(e.target).children().context.id;
+    //     var scrollTop = $(targetId).scrollTop();
+    //     var innerHeight = $(targetId).innerHeight();
+    //     var scrollHeight = $(targetId).prop('scrollHeight');
+    //     if (scrollTop + innerHeight >= scrollHeight) {
+    //         if (targetId == '#p1') {
+    //             location.href='#t2'
+    //         }
+    //         if (targetId == '#p2') {
+    //             location.href='#t3'
+    //         }
+    //         if (targetId == '#p3') {
+    //             location.href='#t4'
+    //         }
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // });
 
     $('.show-sub').click(function() {
         $('#p1-main').css('transform', 'translateX(175%)');    
